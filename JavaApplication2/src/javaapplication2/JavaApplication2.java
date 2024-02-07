7b79727d4b6ee21ffb4e900eb2e6c2441166446a
Biozid Bhuiuan Tonoy <tawsiftonoy007@gmail.com>
2024-02-08 00:11:32 +0600

    change name

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author tonoy
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 50 ;
+        int b = 60 ;
+        System.out.println("tonoy");
+        System.out.println(a+b);
+    }
+    
+}
