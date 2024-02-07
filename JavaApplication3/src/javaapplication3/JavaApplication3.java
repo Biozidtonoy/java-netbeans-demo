6c1501279bea1c6f2438139499adc8a13b13462c
Biozid Bhuiuan Tonoy <tawsiftonoy007@gmail.com>
2024-02-08 00:45:10 +0600

    new name add biozid

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author tonoy
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 50 ;
+        int b = 60 ;
+        System.out.println("tonoy");
+        System.out.println("mabruk hossen fahim");
+        System.out.println("biozid bhuiyan");
+        System.out.println(a+b);
+    }
+    
+}
